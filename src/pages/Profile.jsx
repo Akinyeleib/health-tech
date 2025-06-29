3f7713afe212c85b3d7904f430b2899df5b7d030
Akinyeleib <iakinyele3@gmail.com>
2025-06-30 00:45:08 +0100

    replace endpoint call with local storage retrieval

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,32 +6,26 @@ function Profile() {
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const res = await axios.get('/profile', {
-          headers: { Authorization: `Bearer ${token}` }
-        });
-        setProfile(res.data);
-      } catch (err) {
-        setProfile(null);
-      }
-    };
-    fetchProfile();
-  }, [token]);
+    setProfile({
+      phone_number: localStorage.getItem('health-tech-phone-number') || undefined,
+      name:localStorage.getItem('health-tech-full-name') || undefined
+    })
+  })
+  // useEffect(() => {
+  //   const fetchProfile = async () => {
+  //     try {
+  //       const res = await axios.get('/profile', {
+  //         headers: { Authorization: `Bearer ${token}` }
+  //       });
+  //       setProfile(res.data);
+  //     } catch (err) {
+  //       setProfile(null);
+  //     }
+  //   };
+  //   fetchProfile();
+  // }, [token]);
 
   return (
-    // <div className="p-4">
-    //   <h2 className="text-xl font-bold mb-4">User Profile</h2>
-    //   {profile ? (
-    //     <div>
-    //       <p><strong>Name:</strong> {profile.name}</p>
-    //       <p><strong>Email:</strong> {profile.email}</p>
-    //     </div>
-    //   ) : (
-    //     <p className="text-red-600">Unable to load profile.</p>
-    //   )}
-    // </div>
-
     <div className="flex items-center justify-center px-4">
   <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
     <h2 className="text-2xl font-bold text-black-700 mb-6 text-center">User Profile</h2>
@@ -43,8 +37,8 @@ function Profile() {
           <p className="text-lg font-semibold">{profile.name}</p>
         </div>
         <div>
-          <p className="text-sm text-gray-500">Email</p>
-          <p className="text-lg font-semibold">{profile.email}</p>
+          <p className="text-sm text-gray-500">Phone Number</p>
+          <p className="text-lg font-semibold">{profile.phone_number}</p>
         </div>
       </div>
     ) : (
