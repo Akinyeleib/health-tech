e5af5f232e496444e5ebd6500b9f077d6b33e541
Akinyeleib <iakinyele3@gmail.com>
2025-06-30 01:20:04 +0100

    remove commented code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
   };
 
   return (
- <div className="flex items-center justify-center px-4">
+  <div className="flex items-center justify-center px-4">
   <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
     <h2 className="text-2xl font-bold text-center text-black-700 mb-6">Login</h2>
 
@@ -81,84 +81,3 @@ function Login() {
 }
 
 export default Login;
-
-
-
-
-// import { useState } from 'react';
-// import axios from 'axios';
-
-// function Login() {
-//   const [form, setForm] = useState({ email: '', password: '' });
-//   const [message, setMessage] = useState('');
-
-//   const handleChange = (e) => {
-//     setForm({ ...form, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post('/login', form);
-//       localStorage.setItem('token', res.data.token);
-//       setMessage('Login successful');
-//     } catch (err) {
-//       setMessage(err.response?.data?.error || 'Login failed');
-//     }
-//   };
-
-
-//   return (
-//     <div className="flex items-center justify-center m-auto px-4">
-//       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
-//         <h2 className="text-2xl font-bold text-center text-black-700 mb-6">Login</h2>
-
-//         <form onSubmit={handleSubmit} className="space-y-5">
-//           <div>
-//             <label className="block text-gray-700 font-medium mb-1">Email Address</label>
-//             <input
-//               type="email"
-//               name="email"
-//               value={form.email}
-//               onChange={handleChange}
-//               placeholder="Enter your email"
-//               required
-//               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-//             />
-//           </div>
-
-//           <div>
-//             <label className="block text-gray-700 font-medium mb-1">Password</label>
-//             <input
-//               type="password"
-//               name="password"
-//               value={form.password}
-//               onChange={handleChange}
-//               placeholder="Enter your password"
-//               required
-//               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="w-full bg-orange-800 hover:bg-orange-900 text-white font-semibold py-2 rounded-md transition-colors"
-//           >
-//             Login
-//           </button>
-//         </form>
-
-//         {message && (
-//           <p
-//             className={`mt-4 text-sm text-center ${
-//               success ? 'text-green-600' : 'text-red-600'
-//             }`}
-//           >
-//             {message}
-//           </p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
